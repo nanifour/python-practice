f96463e4c4ee443563ea0cfb94b4ee5daf98c089
nanifour <nholloway013@gmail.com>
2021-06-30 23:42:05 -0400

    tuples and arrays

---
+++ tuples_arrays.py
@@ -0,0 +1,12 @@
+#------------Array & Tuple Practice--------
+
+lucky_numbers = [1, 3, 4, 9, 13, 14, 19]
+
+friends = ["garlic press", "burger", "spam"]
+friends.append("Fit men cook")
+friends.insert(5, "sempre")
+friends.insert(1, "siracha and hot sauce")
+print(friends)
+
+#tuple is for data that is never gonna change 
+coordinates = (4, 5)
